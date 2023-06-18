f098e656d9d8bfe44e1d1b96643b9248b938f2e7
Leon Radley <leon@locka.com>
2023-06-18 10:15:52 +0200

    tailwind
    Signed-off-by: Leon Radley <leon@locka.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'ngstd';
-}
+export class AppComponent {}
